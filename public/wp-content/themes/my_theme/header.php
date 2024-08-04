2832c415140d4fcdb70ba14ed2e2a90b6cde525d
Falconoff <sokolovserega1982@gmail.com>
2024-08-04 22:15:55 +0300

    add header and footer tmpls

---
+++ public/wp-content/themes/my_theme/header.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="тестове завдання для Глянець" />
+  <meta name="robots" content="тестове завдання, верстка, Глянець" />
+
+  <?php wp_head(); ?>
+
+  <title>test</title>
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.1.0/modern-normalize.min.css" integrity="sha512-wpPYUAdjBVSE4KJnH1VR1HeZfpl1ub8YT/NKx4PuQ5NmX2tKuGu6U/JRp5y+Y8XG2tV+wKQpNHVUX03MfMFn9Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="./css/main.css" /> -->
+</head>
+
+<body id="top">
+  <header>
+    <div class="header-top__wrap">
+      <div class="container">
+        <div class="header-top">
+          <div class="header__contacts">
+            <div class="socials">
+              <span class="socials__title">Ми в соціальних мережах:</span>
+              <ul class="socials__list">
+                <li class="socials__item">
+                  <a class="socials__link" href="#" target="_blank" rel="noopener noreferrer" title="ми в Telegram" aria-label="посилання на нас у Telegram">
+                    <svg class="socials__icon" width="22" height="18">
+                      <use href="./icons/sprite.svg#telegram"></use>
+                    </svg>
+                  </a>
+                </li>
+                <li class="socials__item">
+                  <a class="socials__link" href="#" target="_blank" rel="noopener noreferrer" title="ми в Facebook" aria-label="посилання на нас у Facebook"><svg class="socials__icon" width="10" height="22">
+                      <use href="./icons/sprite.svg#icon-fb"></use>
+                    </svg></a>
+                </li>
+                <li class="socials__item">
+                  <a class="socials__link" href="#" target="_blank" rel="noopener noreferrer" title="ми в Instagram" aria-label="посилання на нас у Instagram"><svg class="socials__icon" width="22" height="22">
+                      <use href="./icons/sprite.svg#instagram"></use>
+                    </svg></a>
+                </li>
+              </ul>
+            </div>
+            <div class="phone">
+              <a class="phone__number" href="tel:+380991112233" title="подзвонити">
+                <svg class="phone__icon" width="24" height="24">
+                  <use href="./icons/sprite.svg#smartphone"></use>
+                </svg>
+                +38 099 111 22 33
+                <svg class="arrow__icon" width="14" height="8">
+                  <use href="./icons/sprite.svg#arrow-down"></use>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <nav>
+            <ul class="nav__list">
+              <li class="nav__item nav__item--active">
+                <a href="#">головна</a>
+              </li>
+              <li class="nav__item"><a href="#promo">акції</a></li>
+              <li class="nav__item">
+                <a href="#delivery">доставка та оплата</a>
+              </li>
+              <li class="nav__item"><a href="#about-us">про нас</a></li>
+              <li class="nav__item"><a href="#contacts">контакти</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="header-bottom">
+        <ul class="categories-nav">
+          <li class="categories-nav__item">
+            <a href="#" class="category__link">
+              дивани<svg class="arrow__icon" width="14" height="10">
+                <use href="./icons/sprite.svg#arrow-down"></use>
+              </svg>
+            </a>
+          </li>
+          <li class="categories-nav__item">
+            <a href="#" class="category__link">
+              ліжка<svg class="arrow__icon" width="14" height="10">
+                <use href="./icons/sprite.svg#arrow-down"></use>
+              </svg>
+            </a>
+          </li>
+          <li class="categories-nav__item">
+            <a href="#" class="category__link">
+              крісла<svg class="arrow__icon" width="14" height="10">
+                <use href="./icons/sprite.svg#arrow-down"></use>
+              </svg>
+            </a>
+          </li>
+          <li class="categories-nav__item">
+            <a href="#" class="category__link">
+              комоди<svg class="arrow__icon" width="14" height="10">
+                <use href="./icons/sprite.svg#arrow-down"></use>
+              </svg>
+            </a>
+          </li>
+          <li class="categories-nav__item">
+            <a href="#" class="category__link">
+              шафи<svg class="arrow__icon" width="14" height="10">
+                <use href="./icons/sprite.svg#arrow-down"></use>
+              </svg>
+            </a>
+          </li>
+          <li class="categories-nav__item">
+            <a href="#" class="category__link">
+              кухня<svg class="arrow__icon" width="14" height="10">
+                <use href="./icons/sprite.svg#arrow-down"></use>
+              </svg>
+            </a>
+          </li>
+        </ul>
+        <a href="#" class="logo" title="на головну" aria-label="посилання на головну сторінку"><img src="./images/logo-black.png" alt="logo Furniture test" /></a>
+        <div class="wrapper">
+          <a href="#" class="cart-btn">
+            <svg class="shopping-cart__icon" width="30" height="30">
+              <use href="./icons/sprite.svg#shopping-cart"></use>
+            </svg>кошик
+          </a>
+          <button class="button--accent-color" type="button">
+            замовити дзвінок
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
